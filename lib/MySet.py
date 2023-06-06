59b7a3166db1c5fee387814a1c21d013d5b2f3f5
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-06-06 19:57:10 +0300

    done

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,42 @@
 class MySet:
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
 
-    pass
+    def has(self,value):
+        return value in self.dictionary
+    
+    def __str__(self):
+        set_list=[]
+        for key,value in self.dictionary.items():
+            set_list.append(str(key))
+
+        return f'MySet: {{{",".join(set_list)}}}'
+
+    def add(self,value):
+        self.dictionary[value]=True
+        return self
+    
+    def delete(self,value):
+        self.dictionary.pop(value,None)
+        return self
+
+    def size(self):
+        len(self.dictionary)
+
+    def clear(self):
+        self.dictionary.clear()
+    
+    
+
+    # def first_repeated_value(list):
+    #     for i in range(0,len(list)):
+    #         for j in range(i+1, len(list)):
+    #             if list[i]==list[j]:
+    #                 return list[i]
+    #     return None
+    
+    # print(first_repeated_value([1,2,2,3,2,4,5,6,7,7]))
+
+   
